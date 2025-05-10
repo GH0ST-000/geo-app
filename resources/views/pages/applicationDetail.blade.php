546125e65de5cce7e0d06d99d9a30a718efcfa38
luka.datunashvili1 <luka.datunashvili@agronnect.com>
2025-05-10 23:10:52 +0400

    application module

--- resources/views/pages/applicationDetail.blade.php
+++ resources/views/pages/applicationDetail.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-{{--    @dd($imageFiles)--}}
+{{--    @dd($otherFiles)--}}
     <div class="mb-3 mt-3">
         <span>განცხადების დეტალური გვერდი</span>
     </div>
@@ -61,6 +61,44 @@
             @endif
         </div>
     </div>
+
+
+    <div class="mb-3 mt-3">
+        <span>სხვა ფაილები</span>
+    </div>
+
+    <div class="card">
+        <div class="card-body">
+            <div class="container">
+                <div class="row">
+                    @if(sizeof($otherFiles) > 0 )
+                        @foreach($otherFiles as $otherFile)
+                            <div class="col-sm-3 col-12">
+                                <div class="card">
+                                    <a class="card-body" href="{{$otherFile->file_url}}" target="_blank">
+                                       <div class="d-flex justify-content-between align-items-center"></div>
+                                        <span style="" class=" text-success">{{$otherFile->file_name}}</span>
+                                    </a>
+                                </div>
+                            </div>
+                        @endforeach
+                </div>
+            </div>
+
+            @else
+                <div class="d-flex text-center justify-content-center">
+                    <span class="text-danger">მოცემულ განაცხადზე ფაილები არ მოიძებნა</span>
+                </div>
+            @endif
+        </div>
+    </div>
+<div class="mt-3 mb-3 card">
+    <div class="card-body">
+        <div class="d-flex justify-content-end">
+            <button class="btn btn-success">დადასურება</button>
+        </div>
+    </div>
+</div>
     <script>
         lightbox.option({
             'resizeDuration': 200,
